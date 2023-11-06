2157ba2260b200e9f949d0a1641f2e70b574df51
Akash742 <akashdey742@gmail.com>
2023-11-06 11:43:16 +0530

    fix map sync on both views

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
-import {useState} from 'react';
+import { useCallback, useState, useEffect } from "react";
 import DeckGL from "@deck.gl/react/typed";
 import { ScatterplotLayer } from "@deck.gl/layers/typed";
+import { MapView, WebMercatorViewport } from "@deck.gl/core/typed";
+import { MVTLayer } from "@deck.gl/geo-layers/typed";
+
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoibmVlbGR1dHRhMTkiLCJhIjoiY2tweG9mN3F4MThrNTJ4cDk0enVjcTN4dCJ9.uxa_h0rjqumTxFMI1QELKQ";
 
-import { MVTLayer } from "@deck.gl/geo-layers/typed";
-import StaticMap from 'react-map-gl';
-
-const INITIAL_VIEW_STATE = {
-  longitude: 88,
-  latitude: 22,
-  zoom: 13,
-  pitch: 0,
-  bearing: 0,
-};
-
 function App() {
-  const [viewState, setViewState] = useState(INITIAL_VIEW_STATE);
+  const [viewStates, setViewStates] = useState({
+    main: {
+      longitude: 88,
+      latitude: 22,
+      zoom: 10,
+      pitch: 0,
+      bearing: 0,
+    },
+    minimap: {
+      longitude: 88,
+      latitude: 22,
+      zoom: 10,
+      pitch: 0,
+      bearing: 0,
+    },
+  });
+
   const myLayer = new ScatterplotLayer({
     id: "myLayer",
     data: [
@@ -28,38 +36,41 @@ function App() {
     getColor: [255, 0, 0],
   });
   const myLayer2 = new MVTLayer({
-    data: `https://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v7/{z}/{x}/{y}.vector.pbf?access_token=${MAPBOX_ACCESS_TOKEN}`,
+    id: "map",
+    data: `https://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v8/{z}/{x}/{y}.vector.pbf?access_token=${MAPBOX_ACCESS_TOKEN}`,
     minZoom: 0,
     maxZoom: 23,
     getLineColor: [192, 192, 192],
     getFillColor: [140, 170, 180],
   });
-  const handleViewStateChange = ({viewState}) => {
-    setViewState(viewState);
-  }
+
+  const onViewStateChange = useCallback(({ viewId, viewState }) => {
+    setViewStates({
+      main: viewState,
+      minimap: viewState,
+    });
+  }, []);
+
   return (
-    <div style={{ position: 'relative', height: '100vh', width: '100vw' }}>
-      <DeckGL
-        viewState={viewState}
-        controller={true}
-        layers={[myLayer]}
-        onViewStateChange={handleViewStateChange}
-        style={{ position: 'absolute', height: '100%', width: '100%' }}
-      >
-        <StaticMap accessToken={MAPBOX_ACCESS_TOKEN}/>
-      </DeckGL>
-      <DeckGL
-        viewState={viewState}
-        controller={true}
-        layers={[myLayer2]}
-        onViewStateChange={handleViewStateChange}
-        style={{ position: 'absolute', bottom: '10px', right: '10px', height: '200px', width: '200px' }}
-      >
-         <StaticMap accessToken={MAPBOX_ACCESS_TOKEN}/>
-      </DeckGL>
-    </div>
+    <DeckGL
+      views={[
+        new MapView({
+          id: "main",
+          width: "50%",
+          x: "50%",
+          controller: true,
+        }),
+        new MapView({
+          id: "minimap",
+          width: "50%",
+          controller: true,
+        }),
+      ]}
+      viewState={viewStates.main}
+      layers={[myLayer, myLayer2]}
+      onViewStateChange={onViewStateChange}
+    ></DeckGL>
   );
 }
 
 export default App;
-
