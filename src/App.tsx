862405f465f88145d56dca7a898704f509f2d94d
harshalpatil7372 <harshalpatil7372@gmail.com>
2023-11-21 14:58:07 +0530

    fix masking issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import DeckGL from "@deck.gl/react/typed";
-import { ScatterplotLayer, SolidPolygonLayer } from "@deck.gl/layers/typed";
+import { ScatterplotLayer, SolidPolygonLayer, BitmapLayer } from "@deck.gl/layers/typed";
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoibmVlbGR1dHRhMTkiLCJhIjoiY2tweG9mN3F4MThrNTJ4cDk0enVjcTN4dCJ9.uxa_h0rjqumTxFMI1QELKQ";
 
-import { MVTLayer } from "@deck.gl/geo-layers/typed";
+import { TileLayer, } from "@deck.gl/geo-layers/typed";
 import { MaskExtension } from "@deck.gl/extensions/typed";
 import { useState } from "react";
 
 const INITIAL_VIEW_STATE = {
-  longitude: 88,
-  latitude: 22,
+  longitude: 76.6206927900588,
+  latitude: 11.551920204973305,
   zoom: 13,
   pitch: 0,
   bearing: 0,
@@ -20,6 +20,7 @@ const getPolygon = (lat: number, lng: number, size: number) => [
   [lat + size * 0.5, lng + size * 0.5],
   [lat - size * 0.5, lng + size * 0.5],
 ];
+const tileUrl = "https://cog-nk.kesowa.com/cog/tiles/{z}/{x}/{y}.png?url=http://172.17.0.1:5151/raster/246a6dd2-cb38-409a-bd57-d2ef82d30757.tif";
 function App() {
   const myLayer = new ScatterplotLayer({
     id: "myLayer",
@@ -39,14 +40,21 @@ function App() {
     getPolygon: (d) => d,
     getColor: [255, 0, 0],
   });
-  const myLayer2 = new MVTLayer({
-    data: `https://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v7/{z}/{x}/{y}.vector.pbf?access_token=${MAPBOX_ACCESS_TOKEN}`,
+  const myLayer2 = new TileLayer({
+    data: tileUrl,
     minZoom: 0,
     maxZoom: 23,
-    getLineColor: [192, 192, 192],
-    getFillColor: [140, 170, 180],
+    tileSize: 256,
     extensions: [new MaskExtension()],
     maskId: maskLayer.id,
+    renderSubLayers: props => {
+      const { bbox: { west, south, east, north } } = props.tile;
+      return new BitmapLayer(props, {
+        data: null,
+        image: props.data,
+        bounds: [west, south, east, north]
+      })
+    }
   });
   return (
     <div style={{ position: "relative", height: "100vh", width: "100vw" }}>
