16e5012d7f42bdcd863f4e509b67f010a85652c0
Akash742 <akashdey742@gmail.com>
2023-11-29 16:28:16 +0530

    implemented wind-layer using IconLayer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import DeckGL from "@deck.gl/react/typed";
-import { ScatterplotLayer, SolidPolygonLayer, BitmapLayer } from "@deck.gl/layers/typed";
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoibmVlbGR1dHRhMTkiLCJhIjoiY2tweG9mN3F4MThrNTJ4cDk0enVjcTN4dCJ9.uxa_h0rjqumTxFMI1QELKQ";
 
-import { TileLayer, } from "@deck.gl/geo-layers/typed";
-import { MaskExtension } from "@deck.gl/extensions/typed";
+import { IconLayer } from '@deck.gl/layers/typed';
 import { useState } from "react";
 
 const INITIAL_VIEW_STATE = {
@@ -39,49 +37,30 @@ const getPolygon = (lat: number, lng: number, size: number) => [
 ];
 const tileUrl = "https://cog-nk.kesowa.com/cog/tiles/{z}/{x}/{y}.png?url=http://172.17.0.1:5151/raster/246a6dd2-cb38-409a-bd57-d2ef82d30757.tif";
 function App() {
-  const myLayer = new ScatterplotLayer({
-    id: "myLayer",
-    data: [
-      { position: [88, 22], size: 100 },
-      { position: [22, 88], size: 100 },
-    ],
-    geoPosition: (d) => d.position,
-    getRadius: (d) => d.size,
-    getColor: [255, 0, 0],
-  });
-  const [polygon, setPolygon] = useState(getPolygon(88, 22, 1));
-  const maskLayer = new SolidPolygonLayer({
-    id: "geofence",
-    operation: "mask",
-    data: [polygon],
-    getPolygon: (d) => d,
-    getColor: [255, 0, 0],
-  });
-  const myLayer2 = new TileLayer({
-    data: tileUrl,
-    minZoom: 0,
-    maxZoom: 23,
-    tileSize: 256,
-    extensions: [new MaskExtension()],
-    maskId: maskLayer.id,
-    renderSubLayers: props => {
-      const { bbox: { west, south, east, north } } = props.tile;
-      return new BitmapLayer(props, {
-        data: null,
-        image: props.data,
-        bounds: [west, south, east, north]
-      })
-    }
-  });
+  const maxWindSpeed = Math.max(...windData.map(d => d.windSpeed));
 
-  const windLayer = new ScatterplotLayer({
+  const windLayer = new IconLayer({
     id: 'wind-layer',
-    data: windData, // your wind data here
-    getPosition: d => [d.longitude, d.latitude],
-    getRadius: d => d.windSpeed * 1000,
-    getFillColor: d => [255, (d.windSpeed / maxWindSpeed) * 255, 0],
-    getAngle: d => d.windDirection,
+    data: windData,
     pickable: true,
+    iconAtlas: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png',
+    iconMapping: {
+      marker: {x: 0, y: 0, width: 128, height: 128, mask: true}, 
+      marker2: {x: 128, y: 0, width: 128, height: 128, mask: true}, 
+      marker3: {x: 256, y: 0, width: 128, height: 128, mask: true}, 
+    },
+    getPosition: d => [d.longitude, d.latitude],
+    getIcon: d => {
+      if (d.windSpeed < 5) {
+        return 'marker';
+      } else if (d.windSpeed < 10) {
+        return 'marker2';
+      } else {
+        return 'marker3';
+      }
+    },
+    getSize: d => d.windSpeed,
+    getColor: d => [255, (d.windSpeed / maxWindSpeed) * 255, 0],
   });
   
   return (
@@ -89,14 +68,13 @@ function App() {
       <DeckGL
         initialViewState={INITIAL_VIEW_STATE}
         controller={true}
-        layers={[maskLayer, myLayer, myLayer2, windLayer]}
+        layers={[windLayer]}
         style={{ position: "absolute", height: "100%", width: "100%" }}
         onHover={(info) => {
           const coords = getPolygon(info.x, info.y, 150);
           const mapCoords = coords.map((coord) =>
             info.viewport?.unproject(coord)
           );
-          setPolygon(mapCoords);
         }}
       ></DeckGL>
     </div>
