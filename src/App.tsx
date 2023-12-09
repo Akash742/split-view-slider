929db59daf97b634cbf11591f8c6f5587cf17099
Akash742 <akashdey742@gmail.com>
2023-12-09 10:50:42 +0530

    Fix Pixelation Issue and angle and also changed the arrow-icon

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import DeckGL from "@deck.gl/react/typed";
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoibmVlbGR1dHRhMTkiLCJhIjoiY2tweG9mN3F4MThrNTJ4cDk0enVjcTN4dCJ9.uxa_h0rjqumTxFMI1QELKQ";
 
-import { IconLayer } from '@deck.gl/layers/typed';
+import { IconLayer } from "@deck.gl/layers/typed";
 import { useState } from "react";
-import rightArrow from "../src/assets/right arrow.png"
 
 const INITIAL_VIEW_STATE = {
   longitude: 76.6206927900588,
@@ -48,6 +47,25 @@ const windData = [
   // ... more data points
 ];
 
+function createSVGIcon(idx: any, color: string) {
+  return `<svg fill="${color}" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+  width="800px" height="800px" viewBox="0 0 490.939 490.939"
+  xml:space="preserve">
+ <g>
+  <g>
+  <path fill="${color}" d="M41.552,490.939c-2.665,0-5.297-1.064-7.237-3.095c-3.185-3.337-3.675-8.419-1.186-12.303L181.221,244.52L33.149,15.43
+    c-2.508-3.88-2.031-8.973,1.152-12.32c3.183-3.347,8.245-4.081,12.246-1.768l407.844,235.47c3.094,1.786,5,5.088,5,8.66
+    s-1.906,6.874-5,8.66L46.548,489.6C44.986,490.5,43.263,490.939,41.552,490.939z M72.451,39.39l129.061,199.679
+    c2.128,3.293,2.136,7.525,0.02,10.825L72.156,451.721l357.236-206.25L72.451,39.39z"/>
+  </g>
+ </g>
+ </svg>`;
+ }
+
+function svgToDataURL(svg: any) {
+  return `data:image/svg+xml;charset=utf-8,${encodeURIComponent(svg)}`;
+}
+
 const getPolygon = (lat: number, lng: number, size: number) => [
   [lat - size * 0.5, lng - size * 0.5],
   [lat + size * 0.5, lng - size * 0.5],
@@ -56,31 +74,37 @@ const getPolygon = (lat: number, lng: number, size: number) => [
 ];
 // const tileUrl = "https://cog-nk.kesowa.com/cog/tiles/{z}/{x}/{y}.png?url=http://172.17.0.1:5151/raster/246a6dd2-cb38-409a-bd57-d2ef82d30757.tif";
 function App() {
-  const maxWindSpeed = Math.max(...windData.map(d => d.windSpeed));
+  const maxWindSpeed = Math.max(...windData.map((d) => d.windSpeed));
 
   const windLayer = new IconLayer({
-    id: 'wind-layer',
+    id: "wind-layer",
     data: windData,
     pickable: true,
     // iconAtlas: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png',
-    iconAtlas: rightArrow,  //This is how it is implemented on Aru-Client app. But here it's not working.
+    //iconAtlas: rightArrow,  //This is how it is implemented on Aru-Client app. But here it's not working.
     iconMapping: {
-      marker: {x: 0, y: 0, width: 1024, height: 1024, mask: true}, 
-      // marker2: {x: 128, y: 0, width: 128, height: 128, mask: true}, 
-      // marker3: {x: 256, y: 0, width: 128, height: 128, mask: true}, 
+      marker: { x: 0, y: 0, width: 1024, height: 1024, mask: true },
+      // marker2: {x: 128, y: 0, width: 128, height: 128, mask: true},
+      // marker3: {x: 256, y: 0, width: 128, height: 128, mask: true},
     },
-    getPosition: d => [d.longitude, d.latitude],
-    getIcon: d => {
-      return 'marker';
-    },
-    getSize: d => d.windSpeed * 10,
-    getColor: d => {
+    getPosition: (d) => [d.longitude, d.latitude],
+    getIcon: (d, { index }) => {
       const t = d.windSpeed / maxWindSpeed;
-      return[ 255 * (1 - t), 255 * t, 0];
+      const color = [255 * (1 - t), 255 * t, 0];
+      return {
+        url: svgToDataURL(createSVGIcon(index, `rgb(${color})`)),
+        width: 1024,
+        height: 1024,
+      };
+     },
+    getSize: (d) => d.windSpeed * 10,
+    getColor: (d) => {
+      const t = d.windSpeed / maxWindSpeed;
+      return [255 * (1 - t), 255 * t, 0];
     },
-    getAngle: d => d.windDirection,
+    getAngle: (d) => 360 - d.windDirection,
   });
-  
+
   return (
     <div style={{ position: "relative", height: "100vh", width: "100vw" }}>
       <DeckGL
